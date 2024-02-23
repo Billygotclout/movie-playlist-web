12eae9075c33701c9e8e619800f0b13b7259568a
billygotclout <williamsoluwademilade@gmail.com>
2024-02-23 14:20:34 +0100

    fix

--- src/pages/MoviePlaylist.jsx
+++ src/pages/MoviePlaylist.jsx
@@ -18,7 +18,7 @@ const MoviePlaylist = () => {
           },
         }
       );
-
+      console.log(response.data.results);
       setMovies(response.data.results);
     };
     fetchMovies();
@@ -29,7 +29,8 @@ const MoviePlaylist = () => {
 
     if (!isDuplicate) {
       // If the movie is not already in the playlist, add it
-      setPlaylist([...playlist, movie]);
+      const { id, poster_path, title, overview } = movie;
+      setPlaylist([...playlist, { id, poster_path, title, overview }]);
     } else {
       console.log("Movie already exists in the playlist.");
     }
@@ -43,22 +44,21 @@ const MoviePlaylist = () => {
     const encodedPlaylist = encodeURIComponent(playlistJSON);
     const link = `${window.location.origin}/playlist?data=${encodedPlaylist}`;
 
-    // const shortenedLink = await axios.post(
-    //   "https://t.ly/api/v1/link/shorten",
-    //   {
-    //     long_url:
-    //       "https://movie-playlist-web.vercel.app/playlist?data=[{%22adult%22:false,%22backdrop_path%22:%22/pRmF6VBsRnvWCbLB9P80UvZFMyK.jpg%22,%22genre_ids%22:[10749,35],%22id%22:1014590,%22original_language%22:%22en%22,%22original_title%22:%22Upgraded%22,%22overview%22:%22Ana%20is%20an%20ambitious%20intern%20dreaming%20of%20a%20career%20in%20the%20art%20world%20while%20trying%20to%20impress%20her%20demanding%20boss%20Claire.%20When%20she%27s%20upgraded%20to%20first%20class%20on%20a%20work%20trip,%20she%20meets%20handsome%20Will,%20who%20mistakes%20Ana%20for%20her%20boss%E2%80%93%20a%20white%20lie%20that%20sets%20off%20a%20glamorous%20chain%20of%20events,%20romance%20and%20opportunity,%20until%20her%20fib%20threatens%20to%20surface.%22,%22popularity%22:388.489,%22poster_path%22:%22/9xn7y63VIpUsIVzSP9fYrqJHyl9.jpg%22,%22release_date%22:%222024-02-07%22,%22title%22:%22Upgraded%22,%22video%22:false,%22vote_average%22:7.402,%22vote_count%22:376}]",
-    //   },
-    //   {
-    //     headers: {
-    //       Accept: "application/json",
-    //       "Content-Type": "application/json",
-    //       Authorization: `Bearer ${import.meta.env.VITE_BITLY_ACCESS_TOKEN}`,
-    //     },
-    //   }
-    // );
-    // console.log(link);
-    // console.log(shortenedLink);
+    const shortenedLink = await axios.post(
+      "https://t.ly/api/v1/link/shorten",
+      {
+        long_url: link,
+      },
+      {
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${import.meta.env.VITE_BITLY_ACCESS_TOKEN}`,
+        },
+      }
+    );
+    console.log(link);
+    console.log(shortenedLink.data);
     setShareableLink(link);
     // navigate(`/playlist?data=${encodedPlaylist}`);
   };
@@ -92,7 +92,12 @@ const MoviePlaylist = () => {
       {shareableLink && (
         <div>
           <h3>Shareable Link:</h3>
-          <a href={shareableLink} target="_blank" rel="noopener noreferrer">
+          <a
+            href={shareableLink}
+            target="_blank"
+            className=""
+            rel="noopener noreferrer"
+          >
             {shareableLink}
           </a>
         </div>
